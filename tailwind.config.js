eb5bf8b348c672fa655bf52a39971671983c829d
seanhillweb <seanhillweb@gmail.com>
2023-08-05 15:41:41 -0500

    feat: update gitignore and tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,12 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        tomato: "#ff6257",
+        slate: "#242742",
+        charcoal: "#36384e",
+        grey: "#9294a0"
+      }
     },
     container: {
       center: true,
