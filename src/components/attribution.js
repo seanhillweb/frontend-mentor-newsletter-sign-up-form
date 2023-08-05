d21e1243165ef58eae698fc8c8a3f91778c3beaa
seanhillweb <seanhillweb@gmail.com>
2023-08-05 17:09:45 -0500

    feat: add attribution component

---
+++ src/components/attribution.js
@@ -0,0 +1,17 @@
+/**
+ * Summary. Attribution for Frontend Mentor and Sean Hill.
+ */
+
+export default function Attribution() {
+  return (
+    <div class="attribution bg-slate text-grey text-center px-4 py-3">
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="text-white underline hover:no-underline focus:no-underline">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="https://www.seanhillweb.com" className="text-white underline hover:no-underline focus:no-underline">Sean Hill</a>.
+    </div>
+  )
+}
+
+
